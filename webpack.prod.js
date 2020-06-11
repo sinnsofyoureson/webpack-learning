8adf12fe56e9d964c935ede47d503dd2f5118ffc
Rinat Moskvin <moskvin@mercurydevelopment.com>
2020-06-11 18:45:48 +0400

    Added optimization in prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,9 @@
 const path = require("path");
 const common = require("./webpack.common");
 const merge = require("webpack-merge");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
@@ -9,5 +11,30 @@ module.exports = merge(common, {
     filename: "[name]-[contentHash]-bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
-  plugins: [new CleanWebpackPlugin()],
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name]-[contentHash].css",
+    }),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+      minify: {
+        removeAttributeQuotes: true,
+        collapseWhitespace: true,
+        removeComments: true,
+      },
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader, // 3. Extract css into files
+          "css-loader",                // 2. Turns css into commonjs
+          "sass-loader",               // 1. Turns sass into css
+        ],
+      },
+    ],
+  },
 });
